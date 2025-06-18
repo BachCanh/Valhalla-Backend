95264e241db9d8ac4b817105258800839ca9c2a0
TuanWoox <z1venttt@gmail.com>
2025-06-18 10:25:41 +0700

    Fix logic

--- DAO/user.DAO.js
+++ DAO/user.DAO.js
@@ -2,7 +2,7 @@ const User = require("../models/user.model");
 class UserDAO {
   async findUserByEmail(email) {
     return await User.findOne({
-      where: { email: "example@example.com" },
+      where: { email: email },
     });
   }
 }
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,12 @@
 const { generateAccessToken } = require("../utils/util");
 const bcrypt = require("bcrypt");
 const UserDAO = require("../DAO/user.DAO");
-
+const COOKIE_OPTIONS = require("../config/cookieOptions");
 module.exports.isEmailAvailable = async (req, res) => {
   try {
     const { email } = req.body;
     if (!email)
       return res.status(400).json({ message: "Email không được để trống" });
-
     const user = await UserDAO.findUserByEmail(email);
     if (user)
       return res.status(409).json({ message: "Email đã có người đăng kí" });
@@ -22,10 +21,13 @@ module.exports.login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = await UserDAO.findUserByEmail(email);
-    if (!user) throw new Error("Không tìm thấy thông tin đăng nhập");
+    const userFound = await UserDAO.findUserByEmail(email);
+    if (!userFound) throw new Error("Không tìm thấy thông tin đăng nhập");
+
+    const user = userFound.get({ plain: true });
     const isMatch = bcrypt.compare(password, user?.password);
     if (!isMatch) throw new Error("Thông tin đăng nhập không chính xác");
+
     const accessToken = generateAccessToken(user);
     res.cookie("accessToken", accessToken, COOKIE_OPTIONS.normal);
     return res.status(200).json({ message: "Đăng nhập thành công" });
